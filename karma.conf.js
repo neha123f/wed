07b0ac70af61d64de05dbb85207a6e2a9dd684d9
neha123f <121794908+neha123f@users.noreply.github.com>
2024-01-23 18:46:57 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,12 @@ module.exports = function(config) {
   config.set({
     browsers: ['Chrome'],
     plugins: ['karma-chrome-launcher'],
+    reporters: ['progress', 'html'],
+
+    htmlReporter: {
+      outputFile: 'test-results/test-report.html',
+      // other configuration options
+    },
     // ... other configurations
   });
 };
